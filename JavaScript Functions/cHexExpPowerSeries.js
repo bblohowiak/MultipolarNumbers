352d7db5f7a1f2814dfddb21aeac8b6a37e607d2
Ben Blohowiak <ben.blohowiak@gmail.com>
2024-12-05 12:00:01 -0500

    Create cHexExpPowerSeries.js
    This function generalizes the complex exponential function to complex hexapolars as per a power series definition such that the non-associativity of consolidation is compensated for by pairwise consolidation of terms with each iteration. It accepts a complex hexapolar number and a maximum number of iterations. Nine iterations are sufficient to generate an approximation of e or e^-1 given a real input; after ~200 iterations, numerical precision is strained with diminishing returns.  
    
    Real and complex inputs have expected outputs; nonpositive real-like hexapolar unit inputs converge to one of two quantities other than e^-1, depending on sign subset. 
    
    Generalizing exp(i(theta)) to exp(s(theta)) returns results that differ from extrapolation of natural exponentiation or repeated multiplication of an imaginary-like hexapolar unit such that differences are most evident if theta is not an integer multiple of pi/2.

---
+++ JavaScript Functions/cHexExpPowerSeries.js
@@ -0,0 +1,58 @@
+function cHexExpPowerSeries(a,b,c,d,n){
+//recommend n<200
+//(0,1,9) yields a good approximation of e; (6,1,9) yields a good approximation of its multiplicative inverse.
+//hexapolar units not in the complex plane converge to values other than e^-1 depending on whether their roots are real-like or imaginary-like.
+//exp(s1.57)-> rootless orthant.  
+
+/*if you don't include the //	d=d%(Math.PI*6);, even cHexExpPowerSeries(0,0,2,1*((6*Math.PI/200)*i),200)) produces after 200 iterations
+0 0
+1 1.0000000005484435
+2 8
+3 5.661078677374039e-10
+
+but if you include the modulo operation, the same arguments produce
+
+0 0
+1 1
+2 0
+3 0
+*/
+	d=d%(Math.PI*6);
+
+function factorial(n){
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+
+
+}
+
+	let consolid=consolidate(0,1,0,0,a,b,c,d);
+
+	for (let i=0; i<n-1;i++){
+	
+
+	let quotient=(cHexMultiply(...exponentiateMe(a,b,c,d,(i+2)),0,(1/(factorial(i+2))),0,0));
+	
+
+	
+	consolid=consolidate(...consolid,...quotient);
+	
+	let newSum=consolid;
+
+	}
+
+	let output=consolid;
+return output;	
+
+}
+
+
+	for (let i=0; i<403; i++){
+
+//beware floating-point decimals! computations with Math.PI evidence errors
+//there are irregularities with s^1 unlike the expected values of s^3.
+//is it "information"?
+console.log(" " +  " " + i, cHexExpPowerSeries(0,0,0,1*((6*Math.PI/400)*i),200));
+//console.log(" " +  " " + i, cHexExpPowerSeries(6,1,0,0,200));
+}
