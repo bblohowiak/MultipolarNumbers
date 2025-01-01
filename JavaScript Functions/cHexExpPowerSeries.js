3196d059a65012c44f82838fcb4fa99715e2443f
Ben Blohowiak <ben.blohowiak@gmail.com>
2025-01-01 18:15:13 -0500

    Update cHexExpPowerSeries.js
    Updated comments to specify relationships between different kinds of argument combinations.

--- JavaScript Functions/cHexExpPowerSeries.js
+++ JavaScript Functions/cHexExpPowerSeries.js
@@ -1,5 +1,5 @@
 function cHexExpPowerSeries(a,b,c,d,n){
-//recommend n<200; diminishing returns after n=17
+//recommend n<200; diminishing returns after n=17 for real-like numbers and fails to achieve diminishing returns for imaginary-like numbers as it fails as n>~200
 //(0,1,17) yields a good approximation of e; (6,1,17) yields a good approximation of its multiplicative inverse.
 //hexapolar units not in the complex plane converge to values other than e^-1 depending on whether their roots are real-like or imaginary-like.
 //exp(s1.57)-> orthant that a signed s under repeat self-multiplication bypasses
@@ -37,6 +37,6 @@ return output;
 	for (let i=0; i<403; i++){
 
 
-//console.log(" " +  " " + i, cHexExpPowerSeries(0,0,0,1*((6*Math.PI/400)*i),17));
+//console.log(" " +  " " + i, cHexExpPowerSeries(0,0,0,1*((6*Math.PI/400)*i),200));
 //console.log(" " +  " " + i, cHexExpPowerSeries(6,1,0,0,17));
 }
