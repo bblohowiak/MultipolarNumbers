b4c7b608c1b823ece8cb45bb09a87982cd66dc67
Ben Blohowiak <ben.blohowiak@gmail.com>
2025-12-18 17:36:49 -0500

    Create fmpModulusEuclidean.js

---
+++ JavaScript Functions/Finite Multipolar/fmpModulusEuclidean.js
@@ -0,0 +1,7 @@
+function fmpModulusEuclidean(a,b,c,d,e){
+//e is the quantity of signs in the multipolar system > 1
+	let euclideanInterval=Math.sqrt((b**2)+(d**2));
+//returns a Real number >=0
+return euclideanInterval;
+
+}
